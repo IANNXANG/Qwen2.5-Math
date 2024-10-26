d0fbfd442e26579114c9854fa31e32a2262ab31e
IANNXANG <yx17180324@gmail.com>
2024-10-26 17:36:58 +0800

    cot

--- evaluation/sh/pure_math_aime24.sh
+++ evaluation/sh/pure_math_aime24.sh
@@ -1,7 +1,7 @@
 set -ex
 
 #MODEL_NAME_OR_PATH=/pubshare/zy/cache/Qwen2.5-Math-1.5B-Instruct
-MODEL_NAME_OR_PATH=$PATH
+MODEL_NAME_OR_PATH=$1
 PROMPT_TYPE="qwen25-math-cot"
 OUTPUT_DIR=${MODEL_NAME_OR_PATH}/math_eval
 
